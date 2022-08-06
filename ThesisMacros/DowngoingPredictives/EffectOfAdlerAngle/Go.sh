9056ecada4b23632446b7307727d604f2557fbd2
Daniel Barrow <danbarrow257@gmail.com>
2022-08-06 22:15:02 +0100

    Added LLH Scan plots etc.

---
+++ ThesisMacros/DowngoingPredictives/EffectOfAdlerAngle/Go.sh
@@ -0,0 +1,4 @@
+root -b -l -q 'Draw.C("SubGeV-elike-1dcy",true)'
+root -b -l -q 'Draw.C("SubGeV-mulike-2dcy",true)'
+root -b -l -q 'Draw.C("SubGeV-elike-1dcy",false)'
+root -b -l -q 'Draw.C("SubGeV-mulike-2dcy",false)'
